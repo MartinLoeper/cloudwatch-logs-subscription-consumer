848b27f6b91818971e2d7b1c8f4a6992bf479a16
Martin <martin.loeper@nesto-software.de>
2018-04-01 04:01:41 +0200

    Add type to mapping

--- src/main/java/com/amazonaws/services/kinesis/connectors/elasticsearch/ElasticsearchEmitter.java
+++ src/main/java/com/amazonaws/services/kinesis/connectors/elasticsearch/ElasticsearchEmitter.java
@@ -164,7 +164,7 @@ public class ElasticsearchEmitter implements IEmitter<ElasticsearchObject> {
         	if(!elasticsearchClient.admin().indices().prepareExists(record.getIndex()).get().isExists()) {
         		// set the timestamp type mapping
             	elasticsearchClient.admin().indices().prepareCreate(record.getIndex())
-            			.addMapping("{\n" +
+            			.addMapping("\"" + record.getType() + "\": {\n" +
             							"\"properties\" : {\n" +
 				                			"\"@timestamp\" : { \"type\" : \"date\", \"format\": \"epoch_millis\" }\n" +
 				                		"}\n" +
